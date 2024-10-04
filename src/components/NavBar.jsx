616da0d6c5555bc1d6e53c91cb2c6570a5efbcfb
shakil12 <tahmidul.work@gmail.com>
2024-10-04 19:52:23 +0600

    navbar logo added and login register buttons added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
 import { ROUTES } from "../routes";
+import logo from '../assets/logos-food3.jpeg';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,9 +15,14 @@ const NavBar = () => {
     <nav className="bg-white-500 p-4 shadow-md z-10 relative">
       <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center lg:px-8">
         {/* Brand Name */}
-        <div className="w-full lg:w-1/2 flex justify-between item-center">
+        <div className="w-full lg:w-1/2 flex item-center">
+        <img 
+    src={logo}  // Replace with your actual logo path
+    alt="Logo"
+    className="w-10 h-10 mr-3"
+  />
           <NavLink to={ROUTES.HOME} className="text-2xl font-bold">
-            Rokomari
+            LearnWith<span className="text-blue-500">Tahmid</span>
           </NavLink>
 
           {/* Hamburger Icon for Small Screens */}
@@ -33,7 +39,7 @@ const NavBar = () => {
         {/* Menu Items */}
         <div className={`w-full lg:w-1/2 ${isOpen ? "block" : "hidden"} lg:block lg:flex items-center justify-between border shadow-md lg:border-none lg:shadow-none`}>
           <ul
-            className={`lg:flex flex-col lg:flex-row lg:space-x-6 justify-between lg:-ml-24 ${
+            className={`lg:flex flex-col lg:flex-row lg:space-x-12 justify-between lg:-ml-16 ${
               isOpen ? "block" : "hidden"
             } lg:block lg:static w-full lg:w-auto bg-white lg:bg-transparent space-y-4 lg:space-y-0 py-4 lg:py-0`}
           >
@@ -50,32 +56,12 @@ const NavBar = () => {
             </li>
             <li>
               <NavLink
-                to={ROUTES.ABOUT}
-                className={({ isActive }) =>
-                  isActive ? "text-blue-500" : "text-gray-600"
-                }
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
                 to={ROUTES.PRODUCT}
                 className={({ isActive }) =>
                   isActive ? "text-blue-500" : "text-gray-600"
                 }
               >
-                Blog
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={ROUTES.FAQ}
-                className={({ isActive }) =>
-                  isActive ? "text-blue-500" : "text-gray-600"
-                }
-              >
-                FAQ
+                Product
               </NavLink>
             </li>
           </ul>
@@ -87,10 +73,10 @@ const NavBar = () => {
             } lg:block lg:static top-auto left-0 w-full lg:w-auto bg-white py-4 lg:py-0 flex items-center`}
           >
             <button className="px-4 py-2 bg-blue-500 text-white rounded w-3/4 lg:w-auto">
-              Buy Book
+              Login
             </button>
             <button className="px-4 py-2 bg-gray-500 text-white rounded w-3/4 lg:w-auto">
-              Sign In
+              Register
             </button>
           </div>
         </div>
